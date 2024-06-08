becff7c7f7e54a5315b87d6976d5416bd7f75ad8
Alibaba <abdulmanafaliu414@gmail.com>
2024-06-08 06:56:49 +0000

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import laptopView from '/images/image-product-desktop.jpg'
 import mobileView from '/images/image-product-mobile.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
